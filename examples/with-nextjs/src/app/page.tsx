63b2ada35d0de3bbd9d28d8d639c57a6d0d827af
Rohit Gawli <rohit.gawli@furtribe.in>
2026-01-30 22:13:48 -0500

    as

--- examples/with-nextjs/src/app/page.tsx
+++ examples/with-nextjs/src/app/page.tsx
@@ -15,12 +15,9 @@ const ExcalidrawWithClientOnly = dynamic(
 export default function Page() {
   return (
     <>
-      <a href="/excalidraw-in-pages">Switch to Pages router</a>
-      <h1 className="page-title">App Router</h1>
       <Script id="load-env-variables" strategy="beforeInteractive">
         {`window["EXCALIDRAW_ASSET_PATH"] = window.origin;`}
       </Script>
-      {/* @ts-expect-error - https://github.com/vercel/next.js/issues/42292 */}
       <ExcalidrawWithClientOnly />
     </>
   );
--- examples/with-nextjs/src/excalidrawWrapper.tsx
+++ examples/with-nextjs/src/excalidrawWrapper.tsx
@@ -1,22 +1,13 @@
 "use client";
-import * as excalidrawLib from "@excalidraw/excalidraw";
 import { Excalidraw } from "@excalidraw/excalidraw";
 
 import "@excalidraw/excalidraw/index.css";
 
-import App from "../../with-script-in-browser/components/ExampleApp";
-
 const ExcalidrawWrapper: React.FC = () => {
   return (
-    <>
-      <App
-        appTitle={"Excalidraw with Nextjs Example"}
-        useCustom={(api: any, args?: any[]) => {}}
-        excalidrawLib={excalidrawLib}
-      >
-        <Excalidraw />
-      </App>
-    </>
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <Excalidraw />
+    </div>
   );
 };
 
